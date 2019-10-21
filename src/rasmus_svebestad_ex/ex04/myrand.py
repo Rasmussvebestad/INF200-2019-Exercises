537dd511a5033ace771e6b66338a1b52d9b7b6ee
Rasmus Svebestad <rasmus.svebestad@hotmail.no>
2019-10-21 11:23:51 +0200

    Finished the LCGRand and started the ListRand class

--- src/rasmus_svebestad_ex/ex04/myrand.py
+++ src/rasmus_svebestad_ex/ex04/myrand.py
@@ -5,18 +5,25 @@ __email__ = 'rasmus.svebestad@nmbu.no'
 
 
 class LCGRand:
-    def __init__(self):
+    def __init__(self, seed):
         self.a = 7 ** 5
         self.m = 2 ** 31 - 1
+        self.seed = seed
 
     def rand(self):
-        r[1] = 
-        r[n + 1] = self.a * r[n] mod(self.m)
+        r = [None]
+        r[1] = self.seed
+        r.append(self.a * r[-1] % self.m)
+        return r[-1]
 
 
 class ListRand:
-    def __init__(self):
-        pass
+    def __init__(self, alist):
+        self.alist = alist
 
     def rand(self):
-        pass
+        if len(self.alist) <= 0:
+            raise RuntimeError
+        else:
+            
+
