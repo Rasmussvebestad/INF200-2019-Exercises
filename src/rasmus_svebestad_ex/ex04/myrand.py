9674d0452da4b7f107f839caf8347020106db74b
Rasmus Svebestad <rasmus.svebestad@hotmail.no>
2019-10-21 10:53:31 +0200

    Created the myrand file

---
+++ src/rasmus_svebestad_ex/ex04/myrand.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Rasmus Svebestad'
+__email__ = 'rasmus.svebestad@nmbu.no'
+
+
+class LCGRand:
+    def __init__(self):
+        self.a = 7 ** 5
+        self.m = 2 ** 31 - 1
+
+    def rand(self):
+        r[1] = 
+        r[n + 1] = self.a * r[n] mod(self.m)
+
+
+class ListRand:
+    def __init__(self):
+        pass
+
+    def rand(self):
+        pass
